0ba7c7410675d127ad40f929bbc3e7cc0830ee82
flu12 <fluturalexandruflorin@gmail.com>
2020-04-14 14:53:54 +0300

    updating our app to redirect at home when user is signed in

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sing-up/sign-in-and-sing-up";
 import { auth, createUserProfileDocument } from './firebase/firebase.util';
 import { setCurrentUser } from './redux/user/user.actions'
-
+import {Redirect} from 'react-router-dom'
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -38,20 +38,33 @@ class App extends React.Component {
   }
 
   render() {
+    const {currentUser} = this.props;
     return (
       <div>
         <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUpPage}/>
+          <Route exact path='/signin' render = {() =>
+            currentUser ? (
+              <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+                )
+          }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
-export default connect (null, mapDispatchToProps )(App);
+});
+export default connect (mapStateToProps, mapDispatchToProps )(App);
